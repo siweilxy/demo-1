a8359ac13070973fc69185fba558c175a5a31b3d
siwei <siweilxy@163.com>
2018-09-09 22:35:13 +0800

    add mybatis test

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import java.util.List;
 import java.io.IOException;
 import java.io.Reader;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.Country;
 
 @SpringBootApplication
 public class Demo1Application {
@@ -18,10 +22,16 @@ public class Demo1Application {
 		try {
 			reader = Resources.getResourceAsReader("mybatis-config.xml");
 			SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+			SqlSession session = sessionFactory.openSession();
+			List<Country> cList = session.selectList("selectAll");
+			for(Country country:cList)
+			{
+				System.out.println(country.getCountryname());
+			}
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("end");
 	}
 }
