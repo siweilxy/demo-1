caf075ad045f0a4152310e37423429c3e74674f2
siwei <siweilxy@163.com>
2018-09-09 20:53:12 +0800

    add mybatis

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);		
+		SpringApplication.run(Demo1Application.class, args);	
+		Reader reader;
+		try {
+			reader = Resources.getResourceAsReader("mybatis-config.xml");
+			SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("end");
 	}
 }
